49e1d057b134a3f8325e6b1bb06e6590b9e8985f
zwang20 <michael_wzq@icloud.com>
2021-11-03 14:48:33 +1100

    add windows support

--- cmds/ping.py
+++ cmds/ping.py
@@ -4,9 +4,9 @@ ping.py
 contains ping()
 """
 
+import os
 import subprocess
 import error
-
 
 async def ping(message, *args):
     # pylint:disable=unused-argument
@@ -24,5 +24,13 @@ async def ping(message, *args):
     if not args:
         raise error.ArgumentRequiredError
 
+    # if windows
+    if os.name == "nt":
+        output = subprocess.check_output(["ping", args[0]]).decode()
+
+    # if unix
+    else:
+        output = subprocess.check_output(["ping", "-c", "4", args[0]]).decode()
+
     # return
-    return f"""```{subprocess.check_output(["ping", args[0], "-c", "5"]).decode()}```"""
+    return f"""```{output}```"""
