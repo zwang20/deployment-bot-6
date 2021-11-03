fbe02541ed9fecaca713e0361fdc15b5a46b2cfe
zwang20 <michael_wzq@icloud.com>
2021-11-03 17:29:19 +1100

    minor changes

--- sync.py
+++ sync.py
@@ -7,11 +7,15 @@ syncs the database
 import os
 import subprocess
 import time
+import datetime
+import logging
 
 os.chdir("db-database")
 while True:
+    logging.info("\t%s Pushing to database", datetime.datetime.now())
     subprocess.call(["git", "add", "-A"])
     subprocess.call(["git", "commit", "-m", "database-update"])
     subprocess.call(["git", "push", "--force"])
+    logging.info("\t%s Pulling from database", datetime.datetime.now())
     subprocess.call(["git", "pull"])
     time.sleep(60)
